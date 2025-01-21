5cdf3aae64fa7f42644bb3f2e6f6b0670f96ece7
yxm <yxm.dev@gmx.ie>
2025-01-21 11:14:33 -0300

    feat: improve 'exec' option

--- sh/opts/exec.sh
+++ sh/opts/exec.sh
@@ -1,22 +1,43 @@
-function exec_(){
-    local something=$1
-    shift
+function exec_() {
+    local target="$1"
     local env=""
+    shift
+
+    declare -a flag_from=()
+    flag_from+=(${FLAGS[--from]})
+    flag_from+=(${FLAGS[--env]})
 
     while [[ $# -gt 0 ]]; do
-        case $1 in
-            -f|--from) env="$2"; shift 2;;
-            *) shift;;
-        esac
+        for flag in "${flag_from[@]}"; do
+            if [[ "$1" == "$flag" ]]; then
+                env="$2"
+                shift 2
+                continue 2
+            fi
+        done
+        shift
     done
 
-    local venv=$(venv_ "$env")
-    source "$venv/bin/activate"
+    local venv_path=$(venv_ "$env")
+    if [[ ! -d "$venv_path" ]]; then
+        error_ "The environment '$(env_ $env)' was not initialized."
+        return 1
+    fi
 
-    if [[ -f $something ]]; then
-        python3 "$something"
+    local root=$(find_ root)
+    local env_file="${root}/.env${env:+.$env}"
+    if [[ -f "$env_file" ]]; then
+        set -a
+        source <(cat "$env_file" | xargs -n1)
+        set +a
+    fi
+
+    activate_ "$env"
+
+    if [[ -f "$target" ]]; then
+        python3 "$target"
     else
-        eval "$something"
+        eval "$target"
     fi
 
     deactivate
